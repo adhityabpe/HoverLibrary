f9ed439830fb099754bc9477eec7743131671e85
edinugroho <edidwinugroho80@gmail.com>
2020-12-18 23:35:32 +0700

    librarian auth

--- config/passport.js
+++ config/passport.js
@@ -1,11 +1,11 @@
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const Member = require("../models/memberModel");
+const Pustakawan = require('../models/pustakawanModel');
 
-const keys = require("./db");
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = keys.key;
+opts.secretOrKey = process.env.KEY;
 
 module.exports = passport => {
     passport.use(
@@ -13,8 +13,14 @@ module.exports = passport => {
             Member.findById(jwt_payload.id).then(member => {
                 if (member) {
                     return done(null, member);
+                }else{
+                    Pustakawan.findById(jwt_payload.id,(err, pustakawan) => {
+                        if (pustakawan) {
+                            return done(null, pustakawan);
+                        }
+                        return done(null, false);
+                    })
                 }
-                return done(null, false);
             }).catch(err => console.log(err));
         })
     );
