9b66de32c77b043bc018256dc3f0613cc0e6d3fe
Raditya Yoga Asditama <64305735+radityaaa43@users.noreply.github.com>
2020-12-18 16:14:57 +0700

    Update validation.js

--- validation.js
+++ validation.js
@@ -24,5 +24,31 @@ const loginValidation = data => {
         return schema.validate(data);
 };
 
+//regispustakawan
+const registerPustakawan = data => {
+    const schema = Joi.object({
+        name: Joi.string().min(6).required(),
+        pustakawan_id: Joi.number().integer().min(6).required(),
+        pustakawan_photo:required(),
+        address: Joi.string().min(6).required(),
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required(),
+        phoneNumber: Joi.number().integer().min(6).required(),
+        gender:Joi.string().min(2).required()
+        });
+        return schema.validate(data);
+    };
+
+//loginpustakawan
+const loginPustakawan = data => {
+    const schema = Joi.object({
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required(),
+    });
+        return schema.validate(data);
+    };
+
 module.exports.registerValidation = registerValidation;
 module.exports.loginValidation = loginValidation;
+module.exports.registerPustakawan = registerPustakawan;
+module.exports.loginPustakawan = loginPustakawan;
